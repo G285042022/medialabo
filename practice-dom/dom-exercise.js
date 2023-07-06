0c384c0d0e8990288cc7d6c1c0784a6930296ea9
G285042022 <228504@st.takushoku-u.ac.jp>
2023-07-06 13:54:15 +0900

    4

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,13 +14,51 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-
+let h2= document.querySelector('h2#ex42');
+let p = document.createElement('p'); 
+p.textContent ='写真表と都市の緯度経度のページです';
+p.style.textEmphasis = 'sesame green'; 
+h2. insertAdjacentElement('afterend', p);
 
 // 練習4-3 写真表作成プログラム
+let students = [
+    {face:"taro.png", name:"拓殖太郎"},
+    {face:"jiro.png", name:"高尾二郎"},
+    {face:"hanako.png", name:"茗荷谷花子"}
+]
 
+// div#placeholder を検索
+let dp = document.querySelector('div#phototable')
+
+// 写真表を1つずつ繰り返し追加
+for (let student of students) {
+    // img 要素を作成
+    let img = document.createElement('img');
+    img.setAttribute('src', student.face);
+
+    // 名前の要素を作成
+    let p = document.createElement('p');
+    //p.textContent = student.name;
+
+    // 写真と名前をまとめた div 要素を作成
+   // d.classList.add('faceAndName');
+    p.insertAdjacentElement('beforeend', img);
+
+    // d をページに追加
+    dp.insertAdjacentElement('beforeend', p);
+}
 
 // 練習4-4 箇条書き削除プログラム
-
+let w = document.querySelectorAll('ul#location>li');
+for(let x of w){
+	x.remove();
+}
 
 // 練習4-5 箇条書き追加プログラム
+let div = document.querySelector('ul#location');
 
+for(let k of data) {
+	let p2 = document.createElement('li');
+	 p2.textContent = k.name+'...:緯度'+k.lat+',経度: '+k.lng;
+	div.insertAdjacentElement('beforeend', p2);
+}
