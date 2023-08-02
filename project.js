50e44a785e3a0a48c9e3f6617082b0cb6399819a
G285042022 <228504@st.takushoku-u.ac.jp>
2023-08-03 06:09:57 +0900

    js

--- project.js
+++ project.js
@@ -45,4 +45,75 @@ let data = {
     "cod": 200
   };
   
-  
+  let ken = document.querySelector('#kensaku'); ken.addEventListener('click',sendRequest); function sendRequest() {
+    let nyu = document.querySelector('select#weather'); let idx = nyu.selectedIndex;
+    console.log(idx);
+    let id ='';
+    switch(idx){ case 1:
+    id =''+ 360630;
+    break; case 2:
+    id = 524901;
+    break; case 3:
+    id = 993800; break;
+    case 4:
+    id = 1816670;
+    break; case 5:
+    id = 1850147;
+    break; case 6:
+    id = 1880252;
+    break; case 7:
+    id = 2147714; break;
+    case 8:
+    id = 2643743;
+    break; case 9:
+    id = 2968815;
+    break; case 10:
+    id = 3451189;
+    break; case 11:
+    id = 5128581; break;
+    case 12:
+    id = 5368361;
+    break; }
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json'; console.log(url);
+    // 通信開始
+    axios.get(url)
+    .then(showResult) .catch(showError) .then(finish);
+    }
+    let kais=0;
+    // 通信が成功した時の処理 function showResult(resp) {
+    // 通信成功 // 通信失敗
+    // 通信の最後の処理
+    kais++;
+    let s,ul,ul2,li,a,b,c,d,e,f,g,h,i; if(kais>1){
+    let kesuli = document.querySelector('ul');
+    kesuli.remove(); }
+    // サーバから送られてきたデータを出力 let data = resp.data;
+    // data が文字列型なら，オブジェクトに変換する if (typeof data === 'string') {
+    data = JSON.parse(data); 
+    // data をコンソールに出力
+    
+    // console.log(data);
+    // data.x を出力
+    //console.log(data.x);
+    s = document.querySelector('div#result');
+    ul = document.createElement('ul');
+    ul2 = document.createElement('ul');
+    li = document.createElement('li');
+    a = document.createElement('li');
+    b = document.createElement('li');
+    c = document.createElement('li');
+    d = document.createElement('li');
+    e = document.createElement('li');
+    f = document.createElement('li');
+    g = document.createElement('li');
+    h = document.createElement('li');
+    i = document.createElement('li');
+    li.textContent = "世界の天気(検索結果 1 件)"; a.textContent = "緯度:"+data.coord.lon; b.textContent = "経度:"+data.coord.lat; c.textContent = "天気:"+data.weather[0].description; d.textContent = "最低気温:"+data.main.temp_min; e.textContent = "最高気温:"+data.main.temp_max; f.textContent = "湿度:"+data.main.humidity; g.textContent = "風速:"+data.wind.speed; h.textContent = "風向き:"+data.wind.deg; i.textContent = "都市名:"+data.name; s.insertAdjacentElement('beforeend',ul2); ul2.insertAdjacentElement('beforeend',li); ul2.insertAdjacentElement('beforeend',ul); ul.insertAdjacentElement('beforeend',a); ul.insertAdjacentElement('beforeend',b); ul.insertAdjacentElement('beforeend',c); ul.insertAdjacentElement('beforeend',d); ul.insertAdjacentElement('beforeend',e); ul.insertAdjacentElement('beforeend',f);
+    
+    ul.insertAdjacentElement('beforeend',g); ul.insertAdjacentElement('beforeend',h); ul.insertAdjacentElement('beforeend',i);
+    let ken = document.querySelector('#kensaku'); ken.addEventListener('click',sendRequest);
+    }
+    // 通信エラーが発生した時の処理 function showError(err) {
+    console.log(err); 
+    // 通信の最後にいつも実行する処理 function finish() {
+    console.log('Ajax 通信が終わりました'); 
